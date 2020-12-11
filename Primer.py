67c1c739120b51cc9610e371fce7d5996d7bf081
ElenaMoshkevich <74965841+ElenaMoshkevich@users.noreply.github.com>
2020-12-11 12:40:21 +0300

    Update Primer.py
    проба изменений через веб интерфейс - I am already beginning to understand

--- Primer.py
+++ Primer.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Я уже начинаю понимать')
+    print('I am already beginning to understand')
 
 if __name__ == '__main__':
     main()
